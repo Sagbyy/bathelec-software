97cde0f0f00e5e036365712d3b591776cd521067
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-12-14 17:55:40 +0100

    🔧 refactor(web): rename CreateDerivationForm component and update import paths for consistency

--- apps/web/src/app/(routes)/dashboard/technician/derivations/new/page.tsx
+++ apps/web/src/app/(routes)/dashboard/technician/derivations/new/page.tsx
@@ -1,4 +1,4 @@
-import CreateDerivationForm from '@/components/protected/admin/create-derivation/CreateDerivationForm';
+import CreateDerivationForm from '@/components/protected/admin/create-derivation/create-derivation-form';
 
 export default function CreateDerivationPage() {
   return (
