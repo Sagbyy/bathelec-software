461fc2e6d9b9a14601196ef316de682a8bec98ae
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-05-10 02:24:14 +0200

    ✨ feat(web): configure QueryClient with default options for query stale time and retry settings in QueryProvider

--- apps/web/src/contexts/QueryProvider.tsx
+++ apps/web/src/contexts/QueryProvider.tsx
@@ -4,9 +4,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChildrenProps } from '../types';
 
-export default function QueryProvider({ children }: ChildrenProps) {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 min
+      retry: 1,
+    },
+  },
+});
 
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
