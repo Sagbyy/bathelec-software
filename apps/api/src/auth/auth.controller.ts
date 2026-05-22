7329c288da8d808ff2755c79ce760a076359df7d
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-05-22 17:32:23 +0200

    fix(api): remove sameSite: strict

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -37,8 +37,8 @@ export class AuthController {
 
     res.cookie('token', accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -50,8 +50,8 @@ export class AuthController {
   logout(@Res({ passthrough: true }) res: Response) {
     res.clearCookie('token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
     });
     return { message: 'Logout successful' };
   }
