1f842cedfaa1858e1e824fe718b91833f84ccf95
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-05-23 17:28:15 +0200

    ✨ ci: update CI workflow to include 'develop' branch for continuous integration

--- apps/web/src/app/(routes)/dashboard/layout.tsx
+++ apps/web/src/app/(routes)/dashboard/layout.tsx
@@ -3,23 +3,21 @@
 import { ReactNode } from 'react';
 import { Navbar } from '@/components/protected/navbar';
 import { useAuthGuard } from '@/hooks/use-auth-guard';
+import { useUserStore } from '@/entities/user';
 
 export default function LayoutProtected({ children }: { children: ReactNode }) {
   const { isLoading, isClient, error } = useAuthGuard();
+  const { user } = useUserStore();
 
   if (!isClient) return null;
   if (error) return <p>Error</p>;
 
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Navbar />
-          {children}
-        </>
-      )}
+      {user?.role === 'admin' && <Navbar />}
+      {children}
     </>
   );
 }
